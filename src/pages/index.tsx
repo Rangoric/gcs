c9a891ed38f8fe305ff98c43984a4c069f2694c9
Wilson Gearld Mead III <kem@rangoric.com>
2020-10-20 22:28:45 -0400

    Evergreen & intro page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <>
     <SEO title="Home" />
-    <h1>Welcome To My Personal Site</h1>
-    <p>I am a Professional Full Stack Developer, whatever that happens to mean nowadays.</p>
-    <p>But I also like to write, and also love to game.</p>
-    <p>If you want to know me as a person, you want to read <Link to="/about-me/">About Me</Link><br/></p>
-    <p>If you want to work with me, you want my <Link to="/resume/">Resume</Link></p>
+    <h1>Do you want to play a game?</h1>
+    <p>Here we have the rules for 2 games for now.</p>
+    <p>One is the d20 SRD. This is a good baseline set of rules. You could also do Pathfinder, or Starfinder, or something else. But those rules are elsewhere already and well maintained.</p>
+    <p>The other is my set of rules. It's a mix of everything mentioned above, in addition to things I like, with changes I like.</p>
   </>
 )
 
